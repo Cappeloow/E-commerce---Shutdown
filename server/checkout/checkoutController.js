d9e94adfefe98bcf8f582ee9f5ea4b7947b1f9f8
cappeloow <cappe1996@hotmail.com>
2023-09-12 10:07:11 +0200

    checkout works, however we need to make it sync with the logged in user

--- server/checkout/checkoutController.js
+++ server/checkout/checkoutController.js
@@ -5,21 +5,23 @@ const MY_DOMAIN = "http://localhost:5173";
 
 async function stripeCheckout(req, res) {
   console.log(req.body);
+  const cart = req.body;
+
+  const line_items = cart.map((item) => ({
+    price_data: {
+      currency: "sek",
+      product_data: {
+        name: `New ${item.name}`,
+        description: item.description,
+      },
+      unit_amount: item.price * 100, 
+    },
+    quantity: item.quantity,
+  }));
+
   try {
     const session = await stripe.checkout.sessions.create({
-        line_items:[
-            {
-                price_data:{
-                    currency:"sek",
-                    product_data: {
-                        name: "Keps",
-                        description:"blablablabla.."
-                    },
-                    unit_amount:"20900"
-                },
-                quantity:2
-            }
-        ],
+        line_items,
         mode:"payment",
         success_url:`${MY_DOMAIN}/confirmation`,
         cancel_url:MY_DOMAIN,
