a54650d5833fb36c288183fe49072949ed996354
cappeloow <cappe1996@hotmail.com>
2023-09-14 12:46:44 +0200

    display the products on the confirmation page

--- client/src/pages/Confirmation.tsx
+++ client/src/pages/Confirmation.tsx
@@ -1,28 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function Confirmation() {
-  const retrieveConfirmation = async () => {
-    const id = localStorage.getItem("session-id");
-    console.log(id);
+  const [confirmation, setConfirmation] = useState(null);
+  const [items, setItems] = useState(null);
+  useEffect(() => {
+    const retrieveConfirmation = async () => {
+      const id = localStorage.getItem("session-id");
+      console.log(id);
 
-    const response = await fetch("api/checkout/confirmation", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id }),
-    });
+      const response = await fetch("api/checkout/confirmation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
 
-    if (!response.ok) {
-      return;
-    }
+      if (!response.ok) {
+        return;
+      }
 
-    const data = await response.json();
-    console.log(data);
-  };
+      const data = await response.json();
+      const { retrieveConfirmation, lineItems } = data;
+      setItems(lineItems);
+      setConfirmation(retrieveConfirmation);
+      console.log(data);
+    };
 
-  retrieveConfirmation();
-  return <div>Confirmation</div>;
+    retrieveConfirmation();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <main>
+        {confirmation && (
+          <div>
+            <p>
+              Thank you for the purchase {confirmation.customer_details.name}!
+              {items.data.map((product) => (
+                <div key={product.id}>
+                  <p>
+                    {product.quantity} x {product.description}
+                  </p>
+                </div>
+              ))}
+            </p>
+            <p>
+              Total Summa:
+              {confirmation.amount_total / 100} kr
+            </p>
+          </div>
+        )}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default Confirmation;
